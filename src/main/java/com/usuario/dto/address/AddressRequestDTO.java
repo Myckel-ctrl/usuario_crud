3119ba118072033c662afc25867fc8c029a9c4dd
Myckel de Vasconcelos Mota <myckel.vasconcelos638@gmail.com>
2026-06-20 21:10:52 -0300

    feat: create User, Address, Phone and Auth DTOs

--- src/main/java/com/usuario/dto/address/AddressRequestDTO.java
+++ src/main/java/com/usuario/dto/address/AddressRequestDTO.java
@@ -1,4 +1,23 @@
 package com.usuario.dto.address;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressRequestDTO {
+
+    @NotBlank
+    private String street;
+    @NotBlank private String number;
+    private String complement;
+    @NotBlank private String city;
+    @NotBlank @Size(min = 2, max = 2) private String state;
+    @NotBlank @Size(min = 8, max = 9) private String zipCode;
 }
--- src/main/java/com/usuario/dto/user/UserRequestDTO.java
+++ src/main/java/com/usuario/dto/user/UserRequestDTO.java
@@ -1,4 +1,27 @@
 package com.usuario.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRequestDTO {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
 }
